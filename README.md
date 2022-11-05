1e2a0fdf2dfe898b2ec4a3698dcb0287608abc98
Arthur <esteves.arthur@outlook.com>
2022-11-05 13:05:57 -0300

    Auth Ok

---
+++ README.md
@@ -0,0 +1,6 @@
+# spring-security-roles
+
+As collections pra teste se encontram na pasta resources, que fica na raiz do projeto
+
+Meu blog pesssoal 
+https://medium.com/rapaduratech
---
+++ src/main/java/com/springsecurity/roles/Startup.java
@@ -0,0 +1,18 @@
+package com.springsecurity.roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		Startup.class,
+		Jsr310JpaConverters.class
+})
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+}
